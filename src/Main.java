2bc54f7b2ed4b2a34f0f7f6b8bed5395bc8e5bb3
Jose Angel Silva <jasilva@grupo-orsa.com.mx>
2023-01-16 13:58:14 -0600

    Add: Correccion de .gitignore y modificacion de Main.java
    Se corrige el archivo de gitignore para la correccion de la salida de
    los archivos compilados de Java.
    
    Se crea el programa Main, con el mensaje de Hola Mundo y sobre los tipos
    de comentarios, ademas de como se crea un comentario para la
    documentacion para que sea usado el API de JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+/**
+ * Programa principal del programa
+ * @author Jose Angel Silva Cruz
+ */
 public class Main {
+    /**
+     * Este es una documentacion de la forma de la clase de Java
+     * Funcion principal para la entrada de la aplicacion
+     * @param args
+     * @since 0.01
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Este es un comentario de una sola Linea
+        /*
+        Este es un comentario de multilinea
+         */
     }
 }
