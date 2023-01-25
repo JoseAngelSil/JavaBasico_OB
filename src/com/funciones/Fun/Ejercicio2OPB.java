6c4cea5e085ef017871432abd61ef0d02dbe895f
Jose Angel Silva <jasilva@grupo-orsa.com.mx>
2023-01-25 09:41:00 -0600

    Fix: Solucion de error de src/com/funciones/Fun/Ejercicios2OPB
    Creacion de la funcion getIva() que obtiene de una constante privada IVA
    y de ella es llamada en calculateIVA donde retorna el precio total con
    el impuesto de IVA

--- src/com/funciones/Fun/Ejercicio2OPB.java
+++ src/com/funciones/Fun/Ejercicio2OPB.java
@@ -1,13 +1,18 @@
 package com.funciones.Fun;
 
 public class Ejercicio2OPB {
+    private static final float IVA = 0.16f;
     public static void main(String[] args) {
         System.out.println("Ejercicio2_OPB");
         float precio = 16.50f;
         System.out.printf("Precio Total: $%.3f", calculateIva(precio));
     }
     private static float calculateIva(float price){
-        final float IVA = 0.16f;
-        return (float) price + (price * IVA) ;
+
+        return (float) price + price * getIva() ;
+    }
+
+    public static float getIva() {
+        return IVA;
     }
 }
